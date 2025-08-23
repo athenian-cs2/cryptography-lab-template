67ab7dc6b950b5581cf6f53288971a29dd260698
sthompson-athenian <sthompson@athenian.org>
2025-08-22 17:35:52 -0700

    Update to Java 21 LTS, Gradle 8.10, and modern dependencies with code formatting

--- src/main/java/Caesar.java
+++ src/main/java/Caesar.java
@@ -1,63 +1,60 @@
 import java.util.Scanner;
 
 public class Caesar {
-    // Given a String, encrypts the String with the Caesar cipher
-    // and returns the encrypted String
-    // Ex. encryptCaesar("Hello, World!") => "Khoor, Zruog!"
-    public static String encryptCaesar(String message) {
-        // REPLACE THIS WITH YOUR CODE
-        return message;
+  // Given a String, encrypts the String with the Caesar cipher
+  // and returns the encrypted String
+  // Ex. encryptCaesar("Hello, World!") => "Khoor, Zruog!"
+  public static String encryptCaesar(String message) {
+    // REPLACE THIS WITH YOUR CODE
+    return message;
+  }
+
+  // Given a String, decrypts the String with the Caesar cipher
+  // and returns the original String
+  // Ex. decryptCaesar("Khoor, Zruog!") => "Hello, World!"
+  public static String decryptCaesar(String message) {
+    // REPLACE THIS WITH YOUR CODE
+    return message;
+  }
+
+  // Given a String and a key corresponding to a shift, encrypts
+  // the String using the given key and returns the encrypted String
+  // Ex. encryptCaesarKey("Hello, World!", 5) => "Mjqqt, Btwqi!".
+  public static String encryptCaesarKey(String message, int key) {
+    // REPLACE THIS WITH YOUR CODE
+    return message;
+  }
+
+  // Given a String and a key corresponding to a shift, decrypts
+  // the String using the given key and returns the original String
+  // Ex. decryptCaesarKey("Mjqqt, Btwqi!", 5) => "Hello, World!"
+  public static String decryptCaesarKey(String message, int key) {
+    // REPLACE THIS WITH YOUR CODE
+    return message;
+  }
+
+  // The main method is already provided for you
+  // You do not need to edit this code until Part 2
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+
+    System.out.println("Would you like to \"encrypt\" or \"decrypt\" a message?");
+    String command = scan.nextLine().trim().toLowerCase();
+
+    if (command.equals("encrypt")) {
+      System.out.println("Please enter your message to be encrypted: ");
+      String message = scan.nextLine();
+      System.out.println("Here is your encrypted message: ");
+      System.out.println(encryptCaesar(message));
+    } else if (command.equals("decrypt")) {
+      System.out.println("Please enter your message to be decrypted: ");
+      String message = scan.nextLine();
+      System.out.println("Here is your decrypted message: ");
+      System.out.println(decryptCaesar(message));
+    } else {
+      System.out.println("Unknown command; please type either \"encrypt\" or \"decrypt\"");
     }
 
-    // Given a String, decrypts the String with the Caesar cipher
-    // and returns the original String
-    // Ex. decryptCaesar("Khoor, Zruog!") => "Hello, World!"
-    public static String decryptCaesar(String message) {
-        // REPLACE THIS WITH YOUR CODE
-        return message;
-    }
-
-    // Given a String and a key corresponding to a shift, encrypts
-    // the String using the given key and returns the encrypted String
-    // Ex. encryptCaesarKey("Hello, World!", 5) => "Mjqqt, Btwqi!".
-    public static String encryptCaesarKey(String message, int key) {
-        // REPLACE THIS WITH YOUR CODE
-        return message;
-    }
-
-    // Given a String and a key corresponding to a shift, decrypts
-    // the String using the given key and returns the original String
-    // Ex. decryptCaesarKey("Mjqqt, Btwqi!", 5) => "Hello, World!"
-    public static String decryptCaesarKey(String message, int key) {
-        // REPLACE THIS WITH YOUR CODE
-        return message;
-    }
-
-
-    // The main method is already provided for you
-    // You do not need to edit this code until Part 2
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-
-        System.out.println("Would you like to \"encrypt\" or \"decrypt\" a message?");
-        String command = scan.nextLine().trim().toLowerCase();
-
-        if (command.equals("encrypt")) {
-            System.out.println("Please enter your message to be encrypted: ");
-            String message = scan.nextLine();
-            System.out.println("Here is your encrypted message: ");
-            System.out.println(encryptCaesar(message));
-        }
-        else if (command.equals("decrypt")) {
-            System.out.println("Please enter your message to be decrypted: ");
-            String message = scan.nextLine();
-            System.out.println("Here is your decrypted message: ");
-            System.out.println(decryptCaesar(message));
-        }
-        else {
-            System.out.println("Unknown command; please type either \"encrypt\" or \"decrypt\"");
-        }
-
-        scan.close();
-    }
+    scan.close();
+  }
 }
--- src/test/java/VigenereTest.java
+++ src/test/java/VigenereTest.java
@@ -1,31 +1,77 @@
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.Test;
+
 public class VigenereTest {
-    @Test
-    public void testEncryptVigenere() {
-        // Basic tests
-        assertEquals("HFNOS", Vigenere.encryptVigenere("HELLO", "ABCDE"), "The encrypted output of \"HELLO\" with the key \"ABCDE\" should be: " + "HFNOS");
-        assertEquals("HFNOS WPTOH", Vigenere.encryptVigenere("HELLO WORLD", "ABCDE"), "The encrypted output of \"HELLO WORLD\" with the key \"ABCDE\" should be: " + "HFNOS WPTOH");      
-        assertEquals("HFNOS, WPTOH!", Vigenere.encryptVigenere("HELLO, WORLD!", "ABCDE"), "The encrypted output of \"HELLO, WORLD!\" with the key \"ABCDE\" should be: " + "HFNOS, WPTOH!");    
-        
-        // Harder tests
-        assertEquals("B qlsdk wyy jlwqs fffr krf lrjz dfq", Vigenere.encryptVigenere("A quick fox jumps over the lazy dog", "BARK"), "The encrypted output of \"A quick fox jumps over the lazy dog\" with the key \"BARK\" should be: " + "B qlsdk wyy jlwqs fffr krf lrjz dfq");
-        assertEquals("Wenv af fzi kpxs qwci oznlr wtebsc uinw", Vigenere.encryptVigenere("Pack my box with five dozen liquor jugs", "HELLO"), "The encrypted output of \"Pack my box with five dozen liquor jugs\" with the key \"HELLO\" should be: " + "Wenv af fzi kpxs qwci oznlr wtebsc uinw");      
-        assertEquals("Hfe uskinxtx ehicb lztg zeszzg wumg!", Vigenere.encryptVigenere("How vexingly quick daft zebras jump!", "ARIZONA"), "The encrypted output of \"How vexingly quick daft zebras jump!\" with the key \"ARIZONA\" should be: " + "Hfe uskinxtx ehicb lztg zeszzg wumg!");    
-    }
+  @Test
+  public void testEncryptVigenere() {
+    // Basic tests
+    assertEquals(
+        "HFNOS",
+        Vigenere.encryptVigenere("HELLO", "ABCDE"),
+        "The encrypted output of \"HELLO\" with the key \"ABCDE\" should be: " + "HFNOS");
+    assertEquals(
+        "HFNOS WPTOH",
+        Vigenere.encryptVigenere("HELLO WORLD", "ABCDE"),
+        "The encrypted output of \"HELLO WORLD\" with the key \"ABCDE\" should be: "
+            + "HFNOS WPTOH");
+    assertEquals(
+        "HFNOS, WPTOH!",
+        Vigenere.encryptVigenere("HELLO, WORLD!", "ABCDE"),
+        "The encrypted output of \"HELLO, WORLD!\" with the key \"ABCDE\" should be: "
+            + "HFNOS, WPTOH!");
 
-    @Test
-    public void testDecryptVigenere() {
-        // Basic tests
-        assertEquals("HELLO", Vigenere.decryptVigenere("HFNOS", "ABCDE"), "The decrypted output of \"HFNOS\" with the key \"ABCDE\" should be: " + "HELLO");
-        assertEquals("HELLO WORLD", Vigenere.decryptVigenere("HFNOS WPTOH", "ABCDE"), "The decrypted output of \"HFNOS WPTOH\" with the key \"ABCDE\" should be: " + "HELLO WORLD");
-        assertEquals("HELLO, WORLD!", Vigenere.decryptVigenere("HFNOS, WPTOH!", "ABCDE"), "The decrypted output of \"HFNOS, WPTOH!\" with the key \"ABCDE\" should be: " + "HELLO, WORLD!");
+    // Harder tests
+    assertEquals(
+        "B qlsdk wyy jlwqs fffr krf lrjz dfq",
+        Vigenere.encryptVigenere("A quick fox jumps over the lazy dog", "BARK"),
+        "The encrypted output of \"A quick fox jumps over the lazy dog\" with the key \"BARK\""
+            + " should be: B qlsdk wyy jlwqs fffr krf lrjz dfq");
+    assertEquals(
+        "Wenv af fzi kpxs qwci oznlr wtebsc uinw",
+        Vigenere.encryptVigenere("Pack my box with five dozen liquor jugs", "HELLO"),
+        "The encrypted output of \"Pack my box with five dozen liquor jugs\" with the key \"HELLO\""
+            + " should be: Wenv af fzi kpxs qwci oznlr wtebsc uinw");
+    assertEquals(
+        "Hfe uskinxtx ehicb lztg zeszzg wumg!",
+        Vigenere.encryptVigenere("How vexingly quick daft zebras jump!", "ARIZONA"),
+        "The encrypted output of \"How vexingly quick daft zebras jump!\" with the key \"ARIZONA\""
+            + " should be: Hfe uskinxtx ehicb lztg zeszzg wumg!");
+  }
 
-        // Harder tests
-        assertEquals("A quick fox jumps over the lazy dog", Vigenere.decryptVigenere("B qlsdk wyy jlwqs fffr krf lrjz dfq", "BARK"), "The decrypted output of \"B qlsdk wyy jlwqs fffr krf lrjz dfq\" with the key \"BARK\" should be: " + "A quick fox jumps over the lazy dog");
-        assertEquals("Pack my box with five dozen liquor jugs", Vigenere.decryptVigenere("Wenv af fzi kpxs qwci oznlr wtebsc uinw", "HELLO"), "The decrypted output of \"Wenv af fzi kpxs qwci oznlr wtebsc uinw\" with the key \"HELLO\" should be: " + "Pack my box with five dozen liquor jugs");
-        assertEquals("How vexingly quick daft zebras jump!", Vigenere.decryptVigenere("Hfe uskinxtx ehicb lztg zeszzg wumg!", "ARIZONA"), "The decrypted output of \"Hfe uskinxtx ehicb lztg zeszzg wumg!\" with the key \"ARIZONA\" should be: " + "How vexingly quick daft zebras jump!");
-    }
+  @Test
+  public void testDecryptVigenere() {
+    // Basic tests
+    assertEquals(
+        "HELLO",
+        Vigenere.decryptVigenere("HFNOS", "ABCDE"),
+        "The decrypted output of \"HFNOS\" with the key \"ABCDE\" should be: " + "HELLO");
+    assertEquals(
+        "HELLO WORLD",
+        Vigenere.decryptVigenere("HFNOS WPTOH", "ABCDE"),
+        "The decrypted output of \"HFNOS WPTOH\" with the key \"ABCDE\" should be: "
+            + "HELLO WORLD");
+    assertEquals(
+        "HELLO, WORLD!",
+        Vigenere.decryptVigenere("HFNOS, WPTOH!", "ABCDE"),
+        "The decrypted output of \"HFNOS, WPTOH!\" with the key \"ABCDE\" should be: "
+            + "HELLO, WORLD!");
+
+    // Harder tests
+    assertEquals(
+        "A quick fox jumps over the lazy dog",
+        Vigenere.decryptVigenere("B qlsdk wyy jlwqs fffr krf lrjz dfq", "BARK"),
+        "The decrypted output of \"B qlsdk wyy jlwqs fffr krf lrjz dfq\" with the key \"BARK\""
+            + " should be: A quick fox jumps over the lazy dog");
+    assertEquals(
+        "Pack my box with five dozen liquor jugs",
+        Vigenere.decryptVigenere("Wenv af fzi kpxs qwci oznlr wtebsc uinw", "HELLO"),
+        "The decrypted output of \"Wenv af fzi kpxs qwci oznlr wtebsc uinw\" with the key \"HELLO\""
+            + " should be: Pack my box with five dozen liquor jugs");
+    assertEquals(
+        "How vexingly quick daft zebras jump!",
+        Vigenere.decryptVigenere("Hfe uskinxtx ehicb lztg zeszzg wumg!", "ARIZONA"),
+        "The decrypted output of \"Hfe uskinxtx ehicb lztg zeszzg wumg!\" with the key \"ARIZONA\""
+            + " should be: How vexingly quick daft zebras jump!");
+  }
 }
